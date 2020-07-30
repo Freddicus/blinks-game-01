21b88c97b40912501e2968138edb8a3545974223
Alfred Sterphone <alfred@sterphone.com>
2020-07-30 14:39:05 -0400

    squish

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# blinks-game-01
-Repo for developing my first Blinks game.
+# Make Like A Tree and Leaf
+
+I got this idea looking out the window. 🌳 🤔 💭 
+
+## Concept
+
+* You're growing a tree, starting with soil (seed is implied / buried).
+* Tree components to be incorporated into the growing tree.
+  * Soil
+  * Trunk / Branches
+  * Leaves (budding, grown, dying, dead)
+* Building up the tree meant to be relatively fun / simple, but maintaining the tree meant to be frantic / challenging.
+  * Looking into gameplay loop ideas for this stage
+* Grow the trunk, branches, and add leaves as indicated.
+* Dead leaves need to be pruned, or the tree will become sick.
+* Round is won when the tree is fully grown and maintained for a certain period of time.
+
+## Gameplay
+
+The game is meant for users of a larger amount of Blinks - that number is 18 right now.
+
+### Setup & Tree Phase
+
+The game starts with growing the tree. I'm still working on ideas to gamify this stage. I also might just leave it as is as a fun setup for the gameplay loop.
+
+1. Start by connecting all 18 Blinks.
+2. Double-click one to put them into setup mode.
+3. Once they're all green, detach them so they are all alone.
+4. Take any blink and double click it. This is now your soil.
+5. In five seocnds your soil will sprout.
+6. Add a Blink. This becomes the trunk. Continue to build.
+7. When the tree trunk gets to a certain height, it will branch into two paths.
+8. Add more Blinks as indicated by the growth path.
+
+### Leaf Phase
+
+Once the tree is grown, the gameplay loop begins. I'm still thinking about how this will work and will need to play test. My initial thought is that there are two teams. One team responsible for maintaining the left side of the tree and one responsible for maintaining the right side of the tree.
+
+1. Branch indicates a bud position.
+2. Player attaches ununsed Blink to grow a leaf.
+3. Leaf grows and random rates.
+4. Player must remove dead leaves for fear of harming the tree.
+
+I'm still thinking about the rest. I might want to incorporate more clicking at this point rather than just simply adding and removing leaves for an amount of time.
+
+# About Me
+
+Hello! I'm a professional software engineer and an amateur game designer. What could go wrong? 😂 I live in Northern NJ with my wife and two children. I have *Freddicus Game Studios, LLC* registered in NJ, and I would consider releasing the game under that LLC if given the opportunity.
+
+## Further Discussion
+
+* [Move38 Developer Forum Thread](https://forum.move38.com/t/new-game-wip-make-like-a-tree-and-leaf/549)
---
+++ game-01/game-01.ino
@@ -0,0 +1,326 @@
+/*
+ * Make Like A Tree and Leaf
+ * by Freddicus Game Studios, LLC
+ * 
+ * Author(s): Alfred Sterphone
+ * 
+ * Repo: https://github.com/Freddicus/blinks-game-01
+ * Discussion: https://forum.move38.com/t/new-game-wip-make-like-a-tree-and-leaf/549
+ */
+
+// might be overkill -  moving to regular globals for now
+//// ---- flash mem data ----
+//// #include <avr/pgmspace.h>
+//// const static byte oppositeFaces[] PROGMEM = {3, 4, 5, 0, 1, 2};
+
+// ---- enums ----
+
+enum GameState {
+  SETUP,
+  PLAYING
+};
+
+enum BlinkState {
+  NONE,
+  SOIL,
+  SPROUT,
+  TRUNK,
+  BRANCH,
+  BUD,
+  LEAF
+};
+
+enum LeafState {
+  NAL,  // Not A Leaf
+  YOUNG,
+  MATURE,
+  DYING,
+  DEAD
+};
+
+// --- simple messages ---
+
+enum Message : byte {
+  QUIET,
+  TRUNK_1,
+  TRUNK_2,
+  TRUNK_3,
+  TRUNK_4,
+  TRUNK_5,
+  BRANCH_LEFT_1,
+  BRANCH_LEFT_2,
+  BRANCH_LEFT_3,
+  BRANCH_LEFT_4,
+  BRANCH_RIGHT_1,
+  BRANCH_RIGHT_2,
+  BRANCH_RIGHT_3,
+  BRANCH_RIGHT_4,
+  GROW,
+  GROW_ACK,
+  PLEASE_DETACH
+};
+
+// -------- global constants --------
+
+const static byte oppositeFaces[] = {3, 4, 5, 0, 1, 2};
+
+// -------- global variables --------
+
+byte gameState;
+byte blinkState;
+byte leafState;
+
+uint8_t rearFace;
+uint8_t headFace;
+uint8_t headFaceLeft;
+uint8_t headFaceRight;
+
+bool isTrunkSplit;
+bool sendingGrowth;
+bool receivingGrowth;
+
+bool gotSetupMsg;
+
+Timer soilTimer;
+
+// --- game values ---
+
+#define NUM_BLINKS 18
+#define NUM_PANIC_CLICKS 6
+
+#define COLOR_NONE makeColorRGB(1, 1, 1)         // almost off
+#define COLOR_SOIL makeColorRGB(170, 145, 134)   // i know there is no brown, but...
+#define COLOR_SPROUT makeColorRGB(191, 255, 0)   // lime greenish
+#define COLOR_GROWTH makeColorRGB(84, 164, 222)  // water vibes
+#define COLOR_TRUNK makeColorRGB(255, 192, 0)    // basically orange
+
+#define FACE_SPROUT 0
+
+// --- initialize ---
+
+void setup() {
+  gameState = SETUP;
+  blinkState = NONE;
+  leafState = NAL;
+
+  rearFace = -1;
+  headFace = -1;
+  headFaceLeft = -1;
+  headFaceRight = -1;
+
+  isTrunkSplit = false;
+  sendingGrowth = false;
+  receivingGrowth = false;
+
+  gotSetupMsg = false;
+
+  setColor(dim(WHITE, 40));
+}
+
+// --- game loop ---
+
+void loop() {
+  // game instructions will state to the player to start with all blinks together
+  // then the player will double click a blink to start the game
+  // the player will need to separate all blinks for the game to begin
+
+  if (gameState == SETUP) {
+    // button double clicked - need to tell neighbors to start
+    if (buttonDoubleClicked()) {
+      // we're in setup mode (currently at startup only)
+      // indicate to the user to detach all blinks on all blinks
+      setValueSentOnAllFaces(PLEASE_DETACH);
+
+      // originator got the setup message
+      gotSetupMsg = true;
+
+      // indicate sending setup message
+      setColor(GREEN);
+    }
+
+    FOREACH_FACE(f) {
+      byte faceValue = getLastValueReceivedOnFace(f);
+      bool faceValueExpired = isValueReceivedOnFaceExpired(f);
+
+      // receiving the please detach message
+      if (faceValue == PLEASE_DETACH && !faceValueExpired) {
+        // pass it along to everyone!
+        setValueSentOnAllFaces(PLEASE_DETACH);
+
+        // mark received
+        setColorOnFace(GREEN, f);
+      } else if (faceValue == PLEASE_DETACH && faceValueExpired) {
+        // no longer actively receiving detach message - note and indicate
+        gotSetupMsg = true;
+        setColorOnFace(dim(YELLOW, 40), f);
+      }
+    }
+
+    // if i'm alone, then i'm ready to play
+    if (gotSetupMsg && isAlone()) {
+      gameState = PLAYING;
+      // don't send anything
+      setValueSentOnAllFaces(QUIET);
+    }
+  }
+
+  if (gameState == PLAYING) {
+    switch (blinkState) {
+      case NONE:
+        playingNone();
+        break;
+      case SOIL:
+        playingSoil();
+        break;
+      case SPROUT:
+        playingSprout();
+        break;
+      case TRUNK:
+        playingTrunk();
+        break;
+      case BRANCH:
+        playingBranch();
+        break;
+      case BUD:
+        break;
+      case LEAF:
+        break;
+    }
+
+    updateColor();
+    detectPanic();
+  }
+}
+
+// --------- color section ------------
+
+void updateColor() {
+  switch (blinkState) {
+    case NONE:
+      setColor(COLOR_NONE);
+      break;
+    case SOIL:
+      setColor(COLOR_SOIL);
+      break;
+    case SPROUT:
+      setColorOnFace(COLOR_SPROUT, FACE_SPROUT);
+      handleGrowthColor();
+      break;
+    case TRUNK:
+      setColor(COLOR_TRUNK);
+      handleGrowthColor();
+      break;
+    case BRANCH:
+      setColor(COLOR_TRUNK);
+      handleGrowthColor();
+      break;
+    case BUD:
+      break;
+    case LEAF:
+      break;
+  }
+}
+
+void handleGrowthColor() {
+  if (sendingGrowth) {
+    setColorOnFace(COLOR_GROWTH, headFace);
+  }
+
+  if (receivingGrowth) {
+    setColorOnFace(COLOR_GROWTH, rearFace);
+  }
+}
+
+// -------- Playing methods -------
+
+void playingNone() {
+  if (buttonDoubleClicked()) {
+    blinkState = SOIL;
+    soilTimer.set(5000);
+  }
+
+  // let's find out when we become attached!
+  FOREACH_FACE(f) {
+    byte faceValue = getLastValueReceivedOnFace(f);
+    bool faceValueExpired = isValueReceivedOnFaceExpired(f);
+    if (faceValueExpired) {
+      // continue - nothing to read
+      continue;
+    } else {
+      rearFace = f;
+    }
+
+    if (faceValue >= TRUNK_1 || faceValue < TRUNK_5) {
+      blinkState = TRUNK;
+      headFace = oppositeFaces[f];
+      setValueSentOnFace(faceValue + 1, headFace);
+      break;
+    } else if (faceValue == TRUNK_5) {
+      blinkState = TRUNK;
+      isTrunkSplit = true;
+      headFaceLeft = oppositeFaces[f] - 1;
+      headFaceRight = oppositeFaces[f] + 1;
+      setValueSentOnFace(BRANCH_LEFT_1, headFaceLeft);
+      setValueSentOnFace(BRANCH_RIGHT_1, headFaceRight);
+      break;
+    } else if (faceValue >= BRANCH_LEFT_1 || faceValue < BRANCH_LEFT_4) {
+      blinkState = BRANCH;
+      headFace = oppositeFaces[f];
+      setValueSentOnFace(faceValue + 1, headFace);
+    } else if (faceValue >= BRANCH_RIGHT_1 || faceValue < BRANCH_RIGHT_4) {
+      blinkState = BRANCH;
+      headFace = oppositeFaces[f];
+      setValueSentOnFace(faceValue + 1, headFace);
+    }
+  }
+}
+
+void playingSoil() {
+  // allow undo switch to soil
+  if (buttonDoubleClicked() && isAlone()) {
+    blinkState = NONE;
+  }
+
+  if (soilTimer.isExpired()) {
+    blinkState = SPROUT;
+  } else {
+    // TODO: animate soil about to sprout
+  }
+}
+
+void playingSprout() {
+  headFace = FACE_SPROUT;
+  setValueSentOnFace(TRUNK_1, FACE_SPROUT);
+
+  if (buttonSingleClicked()) {
+    sendingGrowth = true;
+  }
+
+  // send up growth command / lights
+  if (sendingGrowth) {
+    sendGrowth();
+  }
+
+  if (getLastValueReceivedOnFace(headFace) == GROW_ACK) {
+    sendingGrowth = false;
+  }
+}
+
+void playingTrunk() {
+}
+
+void playingBranch() {
+}
+
+// ----- Helpers ---------
+
+void detectPanic() {
+  if (isAlone() && buttonMultiClicked() && buttonClickCount() == NUM_PANIC_CLICKS) {
+    setup();
+    setValueSentOnAllFaces(QUIET);
+  }
+}
+
+void sendGrowth() {
+  setValueSentOnFace(GROW, headFace);
+}
